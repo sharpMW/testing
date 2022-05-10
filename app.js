96240c6a3175b7b551cc17b0376186480a3e3c86
sharpMW <saarimmw@gmail.com>
2022-05-10 10:48:40 +0530

    backend testing

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('bruh');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+//app.use(json.bodyParser());
+app.get('/', (req, res)=>{
+    res.send("heyloo")
+});
+app.listen(3000, ()=>{
+    console.log('bruh');
+})
